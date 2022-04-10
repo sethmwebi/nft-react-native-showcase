e7a024e41a583ab20b816852f106e5009903b7ac
Seth Mwebi <sethmwebi27@gmail.com>
2022-04-10 17:43:04 +0300

    showcase for react-native nft market place built earlier😜

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button"
+import Download from "./Download"
+import Features from "./Features"
+import SectionWrapper from "./SectionWrapper"
+
+export {
+	Download, Features, SectionWrapper
+}
